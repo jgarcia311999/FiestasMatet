07723d7360655d77ffddb813fefa29f2a8c6da57
Jesús García Alemany <jyjmatet@gmail.com>
2025-09-02 10:21:31 +0200

    chore(ideas): update "Prueba 1"

--- src/data/fiestas.ts
+++ src/data/fiestas.ts
@@ -4,6 +4,11 @@ export const ideasProd: IdeasData = [
   {
     key: "sec-ei9ogbpy",
     title: "Prueba 1",
-    items: []
+    items: [
+      {
+        id: "bptaodmx",
+        text: "Comprar muchos globos"
+      }
+    ]
   }
 ] as const;
